aef3c8f19f8bf3ff66c109ed7058fde6b4c98418
michael <contact@michael-neisens.de>
2025-04-22 15:12:03 +0200

    clean up authapp

--- authapp/urls.py
+++ authapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import register, user_login, guest_login, logout_view
 
 urlpatterns = [
-    path('register/',   register,      name='register'),
-    path('login/',      user_login,    name='login'),
-    path('guest-login/', guest_login,   name='guest-login'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('guest-login/', guest_login, name='guest-login'),
     path('logout/', logout_view, name='logout'),
 ]
